a43ca3a2243507e3488771528647b2d272486376
“Vinícius <vini89go@gmail.com>
2020-07-02 18:53:17 -0300

    Change db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_06_23_040355) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
